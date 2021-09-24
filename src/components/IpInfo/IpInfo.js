0291aa57163b9851b3a2728bd30ce95bace8cfae
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-23 23:36:09 -0600

    Set up basic structure for IpForm fetch calls components

--- src/components/IpInfo/IpInfo.js
+++ src/components/IpInfo/IpInfo.js
@@ -2,29 +2,42 @@ import './IpInfo.css';
 import React, { useState, useEffect } from 'react';
 import IpCard from '../IpCard/IpCard';
 import IpForm from '../IpForm/IpForm';
-import { fetchLocalIp } from '../../utils';
+import { fetchLocalIp, fetchAllExternalIp, fetchFieldExternalIp } from '../../utils';
 
 const IpInfo = () => {
-    const [localIp, setLocalIp] = useState({});
-    const [requestedIp, setRequerstedIp] = useState({});
+  const [localIp, setLocalIp] = useState({});
+  const [requestIpAll, setRequestIpAll] = useState({});
+  const [requestIpField, setRequestIpField] = useState({});
 
-    useEffect(() => {
-      getLocalIpInfo()
-    }, [])
+  useEffect(() => {
+    getLocalIpInfo()
+  }, [])
 
-    const getLocalIpInfo = () => {
-      return fetchLocalIp()
-      .then(data => setLocalIp(data))
-      .then(console.log(localIp))
+  const getLocalIpInfo = () => {
+    return fetchLocalIp()
+    .then(data => setLocalIp(data))
+    .then(console.log(localIp))
+  }
+
+  const getRequestedIpInfo = (ipAddress, ipField) => {
+    if(!ipField) {
+      return fetchAllExternalIp(ipAddress)
+      .then(data => setRequestIpAll(data))
+      .then(console.log(requestIpAll))
     }
+    
+    return fetchFieldExternalIp(ipAddress, ipField)
+    .then(data => setRequestIpField(data))
+    .then(console.log(requestIpField))
+  }
 
-    return  (
-      <div className="ipInfo">
-        <h2 className='ipInfo'>IpInfo</h2>
-        <IpForm />
-        <IpCard ip={localIp} />
-      </div>
-    )
+  return  (
+    <div className="ipInfo">
+      <h2 className='ipInfo'>IpInfo</h2>
+      <IpForm getRequestedIpInfo={getRequestedIpInfo}/>
+      <IpCard ip={localIp} />
+    </div>
+  )
 }
 
 export default IpInfo;
