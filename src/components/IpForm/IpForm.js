f146f8977b95f8a7e20ea53514db23230b7413d3
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-24 11:02:34 -0600

    Add array container to create a dynamic dropdown list for ip fields

--- src/components/IpForm/IpForm.js
+++ src/components/IpForm/IpForm.js
@@ -5,6 +5,13 @@ const IpForm = ({getRequestedIpInfo}) => {
 
   const [ipAddress, setIpAddress] = useState("");
   const [ipField, setIpField] = useState("");
+  const ipFieldsContainer = [{Name:'All', Value:'all'}, {Name:'Ip', Value:'ip'}, {Name:'Version', Value:'version'}, 
+  {Name:'City', Value:'city'}, {Name:'Region', Value:'region'}, {Name:'Country Name', Value:'country_name'},
+  {Name:'Country Code', Value:'country_code'}, {Name:'Country Capital', Value:'country_capital'}, 
+  {Name:'Country Calling Code', Value:'country_calling_code'}, {Name:'Country Area', Value:'country_area'},
+  {Name:'Country Population', Vlaue:'country_population'}, {Name:'Country Postal', Value:'postal'},
+  {Name:'Latitude', Value:'latitude'}, {Name:'Longitude', Value:'longitude'},{Name:'Timezone', Value:'timezone'},
+  {Name:'Currency', Value:'currency_name'}, {Name:'Languages', Values:'languages'}, {Name:'ASN', Value:'asn'}, {Name:'Organization', Value:'org'}]
 
   const searchIp = (event) => {
     event.preventDefault();
@@ -32,6 +39,13 @@ const IpForm = ({getRequestedIpInfo}) => {
         value="city"
         onChange={(event) => setIpField(event.target.value)}
        /> City
+      <select onChange={(event) => setIpField(event.target.value)}>
+        {ipFieldsContainer.map(list => (
+          <option value={list.Value}>
+            {list.Name}
+          </option>
+        ))}
+      </select>
       <button onClick={searchIp}>SUBMIT</button>
     </form>
   )
