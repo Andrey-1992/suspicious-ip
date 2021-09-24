6fd2b8f6e26acedeeccc9ffa76f4410f44c44c50
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-24 00:04:36 -0600

    Construct an input filed to search for all fileds from an IP Address

--- src/components/IpForm/IpForm.js
+++ src/components/IpForm/IpForm.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './IpForm.css'
 
 const IpForm = ({getRequestedIpInfo}) => {
-    return  (
-        <h2 className='ipForm'>IpForm</h2>
-    )
+
+  const [ipAddress, setIpAddress] = useState("");
+
+  const searchIp = (event) => {
+    event.preventDefault();
+    getRequestedIpInfo(ipAddress);
+    clearInputs();
+  }
+
+  const clearInputs = () => {
+    setIpAddress("")
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="IP Address"
+        name="ipAddress"
+        value={ipAddress}
+        onChange={(event) => setIpAddress(event.target.value)}
+      />
+      <button onClick={searchIp}>SUBMIT</button>
+    </form>
+  )
 }
 
 export default IpForm;
