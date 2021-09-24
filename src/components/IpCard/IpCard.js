e30c637ebddbb2e2b2ba1f1ce53042648e829f46
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-23 19:29:20 -0600

    Connect IpInfo componenet with fecth calls

--- src/components/IpCard/IpCard.js
+++ src/components/IpCard/IpCard.js
@@ -8,7 +8,24 @@ const IpCard = ({ip}) => {
   }
   return (
     <div className="single-card">
-      <p>{ip}</p>
+      <p>IP: {ip.ip}</p>
+      <p>Version: {ip.version}</p>
+      <p>City: {ip.city}</p>
+      <p>Region: {ip.region}</p>
+      <p>Country Name: {ip.country_name}</p>
+      <p>Country Code: {ip.country_code}</p>
+      <p>Country Capital: {ip.country_capital}</p>
+      <p>Country Calling Code: {ip.country_calling_code}</p>
+      <p>Country Area: {ip.country_area}</p>
+      <p>Country Population: {ip.country_population}</p>
+      <p>Postal: {ip.postal}</p>
+      <p>Latitude: {ip.latitude}</p>
+      <p>Longitude: {ip.longitude}</p>
+      <p>Timezone: {ip.timezone}</p>
+      <p>Currency: {ip.currency_name}</p>
+      <p>Languages: {ip.languages}</p>
+      <p>ASN: {ip.asn}</p>
+      <p>Organization: {ip.org}</p>
     </div>
   );
 }
--- src/components/IpInfo/IpInfo.js
+++ src/components/IpInfo/IpInfo.js
@@ -2,19 +2,20 @@ import './IpInfo.css';
 import React, { useState, useEffect } from 'react';
 import IpCard from '../IpCard/IpCard';
 import IpForm from '../IpForm/IpForm';
-import { fetchLocalIp } from '../../util';
+import { fetchLocalIp } from '../../utils';
 
 const IpInfo = () => {
-    const [localIp, setLocalIp] = useState('');
+    const [localIp, setLocalIp] = useState({});
     const [requestedIp, setRequerstedIp] = useState({});
 
     useEffect(() => {
-      // getLocalIpInfo()
-      setLocalIp('192.152.080.001')
+      getLocalIpInfo()
     }, [])
 
     const getLocalIpInfo = () => {
-
+      return fetchLocalIp()
+      .then(data => setLocalIp(data))
+      .then(console.log(localIp))
     }
 
     return  (
