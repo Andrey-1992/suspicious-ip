fff86f5a55c433b88d02f58d302e5c96b7445822
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-26 17:49:23 -0600

    Style ip report cards

--- src/components/IpCardInfo/IpCardInfo.js
+++ src/components/IpCardInfo/IpCardInfo.js
@@ -8,23 +8,24 @@ const IpCardInfo = ({ip}) => {
   } 
   return (
     <div className="single-card">
-      <p className="ip-card-text">IP: {ip.ip}</p>
-      <p className="ip-card-text">Version: {ip.version}</p>
-      <p className="ip-card-text">City: {ip.city} | Region: {ip.region}</p>
-      <p className="ip-card-text">Country Name: {ip.country_name}</p>
-      <p className="ip-card-text">Country Code: {ip.country_code}</p>
-      <p className="ip-card-text">Country Capital: {ip.country_capital}</p>
-      <p className="ip-card-text">Country Calling Code: {ip.country_calling_code}</p>
-      <p className="ip-card-text">Country Area: {ip.country_area}</p>
-      <p className="ip-card-text">Country Population: {ip.country_population}</p>
-      <p className="ip-card-text">Postal: {ip.postal}</p>
-      <p className="ip-card-text">Latitude: {ip.latitude}</p>
-      <p className="ip-card-text">Longitude: {ip.longitude}</p>
-      <p className="ip-card-text">Timezone: {ip.timezone}</p>
-      <p className="ip-card-text">Currency: {ip.currency_name}</p>
-      <p className="ip-card-text">Languages: {ip.languages}</p>
-      <p className="ip-card-text">ASN: {ip.asn}</p>
-      <p className="ip-card-text">Organization: {ip.org}</p>
+      <p className="ip-card-text"><i class="fas fa-code-branch"></i> <b>IP:</b> <i>{ip.ip}</i></p>
+      <p className="ip-card-text"><i class="fas fa-atlas"></i> <b>Version:</b> <i>{ip.version}</i></p>
+      <p className="ip-card-text"><i class="fas fa-globe-americas"></i> <b>Country Name:</b> <i>{ip.country_name}</i></p>
+      <p className="ip-card-text"><i class="fas fa-code"></i> <b>Country Code:</b> <i>{ip.country_code}</i></p>
+      <p className="ip-card-text"><i class="fas fa-map-signs"></i> <b>Country Capital:</b> <i>{ip.country_capital}</i></p>
+      <p className="ip-card-text"><i class="fas fa-code"></i> <b>Country Calling Code:</b> <i>{ip.country_calling_code}</i></p>
+      <p className="ip-card-text"><i class="fas fa-map-pin"></i> <b>Country Area:</b> <i>{ip.country_area}</i></p>
+      <p className="ip-card-text"><i class="fas fa-user-friends"></i> <b>Country Population:</b> <i>{ip.country_population}</i></p>
+      <p className="ip-card-text"><i class="fab fa-font-awesome-flag"></i> <b>City:</b> <i>{ip.city}</i></p>
+      <p className="ip-card-text"><i class="fas fa-map-marked"></i> <b>Region:</b> <i>{ip.region}</i></p>
+      <p className="ip-card-text"><i class="fas fa-mail-bulk"></i> <b>Postal:</b> <i>{ip.postal}</i></p>
+      <p className="ip-card-text"><i class="fas fa-sort-numeric-up"></i> <b>Latitude:</b> <i>{ip.latitude}</i></p>
+      <p className="ip-card-text"><i class="fas fa-sort-numeric-down-alt"></i> <b>Longitude:</b> <i>{ip.longitude}</i></p>
+      <p className="ip-card-text"><i class="fas fa-hourglass-half"></i> <b>Timezone:</b> <i>{ip.timezone}</i></p>
+      <p className="ip-card-text"><i class="fas fa-money-check-alt"></i> <b>Currency:</b> <i>{ip.currency_name}</i></p>
+      <p className="ip-card-text"><i class="fas fa-language"></i> <b>Languages:</b> <i>{ip.languages}</i></p>
+      <p className="ip-card-text"><i class="fas fa-barcode"></i> <b>ASN:</b> <i>{ip.asn}</i></p>
+      <p className="ip-card-text"><i class="fas fa-sitemap"></i> <b>Organization:</b> <i>{ip.org}</i></p>
     </div>
   )
 }
