7a25ef485fdceaccb4e3f8fa956975c61f176339
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-26 20:38:03 -0600

    Install react elastic carrousel

--- src/components/IpContainer/IpContainer.js
+++ src/components/IpContainer/IpContainer.js
@@ -1,10 +1,33 @@
-import React from 'react';
 import './IpContainer.css'
+import React, { useState, useEffect } from 'react';
+import Carousel from 'react-elastic-carousel';
 
 const IpContainer = () => {
-    return  (
-        <h2 className='ipContainer'>IpContainer</h2>
-    )
+
+    useEffect(() => {
+        retrieveFromStorage()
+      }, [])
+
+  const retrieveFromStorage = () => {
+    const getStoredPosts = Object.keys(localStorage).map(ipData => {
+        return JSON.parse(localStorage.getItem(ipData))
+    // console.log(ipData)
+    })
+    console.log(getStoredPosts);
+    // setFavoritedPosts(getStoredPosts)
+  }
+    
+  const deleteFromStorage = (post) => {
+    localStorage.removeItem(post.id)
+  }
+
+  const createIpCards() {
+
+  }
+
+  return  (
+    <h2 className='ipContainer'>IpContainer</h2>
+  )
 }
 
 export default IpContainer;
