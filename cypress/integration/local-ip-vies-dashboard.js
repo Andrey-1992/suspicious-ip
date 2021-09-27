cebbc4fcabd4329c577a360a5b2fad9223ac8c81
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-27 15:48:13 -0600

    Add test suite for saved window

--- cypress/integration/local-ip-vies-dashboard.js
+++ cypress/integration/local-ip-vies-dashboard.js
@@ -1,27 +1,19 @@
-describe('Local IP view dashboard user flow', () => {
+describe('Saved IP view dashboard user flow', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000/local-ip')
+    cy.visit('http://localhost:3000/saved-ip')
   })
 
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
   })
 
-  it('Local IP view should be visible', () => {
-    cy.get('.ip-card-info').should('be.visible')
-  })
-
-  it('Should display a single card', () => {
-    cy.get('.single-card').should('be.visible')
-  })
-
-  it('Should be display each field of the Local IP', () => {
-    cy.get('.ip-card-text').should('be.visible')
-  })
+  it('Saved IP view should be visible', () => {
+    cy.get('.saved-cards-container').should('be.visible')
+  }) 
 
   it('Local IP should have an card incremenetion when you hover over it', () => {
-    cy.get('.ip-card-info').trigger('mouseover')
+    cy.get('.saved-cards-container').trigger('mouseover')
   })
 
 });
