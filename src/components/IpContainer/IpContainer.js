99de2070793fffffceea41e858d99995321bd5b0
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-09-26 21:19:18 -0600

    Set up data basees

--- src/components/IpContainer/IpContainer.js
+++ src/components/IpContainer/IpContainer.js
@@ -1,28 +1,43 @@
 import './IpContainer.css'
 import React, { useState, useEffect } from 'react';
+import IpSavedCard from '../IpSavedCard/IpSavedCard';
 import Carousel from 'react-elastic-carousel';
 
 const IpContainer = () => {
+  const [ipCardsData, setIpCardsData] = useState([]);
 
-    useEffect(() => {
-        retrieveFromStorage()
-      }, [])
+  useEffect(() => {
+    retrieveFromStorage()
+  }, [])
 
   const retrieveFromStorage = () => {
     const getStoredPosts = Object.keys(localStorage).map(ipData => {
-        return JSON.parse(localStorage.getItem(ipData))
+        return JSON.parse(localStorage.getItem(ipData.props))
     // console.log(ipData)
     })
-    console.log(getStoredPosts);
-    // setFavoritedPosts(getStoredPosts)
+    setIpCardsData(getStoredPosts);
+    console.log(ipCardsData);
+    createIpCards(getStoredPosts)
   }
     
-  const deleteFromStorage = (post) => {
-    localStorage.removeItem(post.id)
+  const deleteFromStorage = (data) => {
+    localStorage.removeItem(data.ip)
   }
 
-  const createIpCards() {
+  const saveToStorage = () => {
+    console.log('test ')
+  }
 
+  const createIpCards = (getStoredPosts) => {
+    const cardsContainer = getStoredPosts.map(info => {
+      return (
+        <IpSavedCard ip={info} />
+       )
+    })
+    console.log("test cardsContainer:", cardsContainer)
+    return (
+        <IpSavedCard ip={getStoredPosts}/>
+    )
   }
 
   return  (
@@ -31,3 +46,8 @@ const IpContainer = () => {
 }
 
 export default IpContainer;
+
+   //   <Carousel>
+        {/* {cardsContainer.map(card => card)} */}
+        // <IpCardInfo ip={getStoredPosts[0]} saveToStorage={saveToStorage}/>
+    //   </Carousel>
